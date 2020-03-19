214627cfea3dc28f6b48e6a9f33e5fa5582bef74
bladas <dashkevich_v@ukr.net>
2020-03-19 23:00:29 +0200

    Up1

--- datawizT3/settings.py
+++ datawizT3/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMidelware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'datawizT3.urls'
